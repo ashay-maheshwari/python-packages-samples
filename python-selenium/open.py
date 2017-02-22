6f7906494f9064b8063b9be4db7774b786f10daa
ashay-maheshwari <ashay@vlabs.ac.in>
2017-02-22 16:50:10 +0530

    add code to visit linkedin

--- python-selenium/open.py
+++ python-selenium/open.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 
+linked_in_url = "https://www.linkedin.com/"
 driver = webdriver.Firefox()
-driver.get("http://www.stackoverflow.com")
+driver.get(linked_in_url)
+
+
+inputElement = driver.find_element_by_id("login-email")
+inputElement.send_keys("aashay")
+
